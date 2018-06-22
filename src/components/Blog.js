f0aeccbac52d00853e2138ccddf22e23adab25c5
Qcpwned07 <qcpwned07@gmail.com>
2018-06-23 00:02:15 +0200

    Added Blog and BlogListItem components

---
+++ src/components/Blog.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { DataTable, TableHeader, TableBody, TableRow, TableColumn, Button } from 'react-md';
+import {BlogListItem} from './BlogListItem';
+
+export class Blog extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <Page>
+
+
+
+        </Page>
+    }
+
+
+}
+
+
---
+++ src/components/BlogListItem.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { DataTable, TableHeader, TableBody, TableRow, TableColumn, Button } from 'react-md';
+
+
+export const BlogListItem()
