bab7c297e5011fa3f8dcf24b0f36bab21c9de966
Qcpwned07 <qcpwned07@gmail.com>
2018-06-29 16:52:14 +0200

    Updated the BlogPostView to fetch the data from the backend

--- src/views/BlogPostView.js
+++ src/views/BlogPostView.js
@@ -15,55 +15,93 @@ import {FacebookShareButton,
     LinkedinIcon,
     TwitterShareButton,
     TwitterIcon} from 'react-share';
+import {DeleteButton, EditButton, WriteButton} from "../components/BlogPostButtons";
+import BlogService from "../services/BlogService";
+import UserService from "../services/UserService";
 
 export class BlogPostView extends React.Component {
 
     constructor(props){
         super(props)
+        this.state = {
+            loading: false,
+            post: {},
+            currentUser: "",
+        }
+
+        //this.content = this.props.post.content.split(".");
+    }
+
+    componentWillMount(props){
+        this.setState({
+            loading: true
+        });
+
+        let idb = this.props.match.params.id;
+        const {id, username} = UserService.getCurrentUser();
+        this.setState({currentUser:username});
+
+        BlogService.getBlog(idb).then((data) => {
+            this.setState({
+                post: data,
+                loading: false
+            });
+        }).catch((e) => {
+            console.error(e);
+        });
+    }
+
+    editPost(id){
+        BlogService.updateBlog(id,UserService.getCurrentUser().username);
+        this.props.history.push('/');
+    }
+
+    deletePost(id){
+        BlogService.deletePost(id).then(
+            this.props.history.push('/')
+        );
     }
 
     render() {
+        if (this.state.loading) {
+            return (<h2>Loading...</h2>);
+        }
+
         const url = "https://facebook.com"
         const shareText = 'Check this out'
         const shareStyle = {display:"inline-block", margin:'5px'}
         const h4Style = {display:"inline-block" ,padding:"0 16px"}
         const h3Style = {marginBottom:"0px",color:"maroon" ,padding:"0 16px"}
+        console.log(this.state)
 
         return (
 
             <Page>
-                <Card style={{width:'90%', margin:'0 5%'}}>
-                    <CardTitle title="FIRST BLOG POST"/>
-                    <h3 style={h3Style}>Matthieu Picard</h3>
-                    <h4 style={h4Style}>07/04/2018</h4>
+                <WriteButton active={UserService.isAuthenticated()}/>
+                {this.state.currentUser === this.state.post.authorUsername && <EditButton post={this.state.post}/>}
+                {this.state.currentUser === this.state.post.authorUsername && <DeleteButton id={this.state.post._id}/>}
+                <Card style={{width:'90%', margin:'70px 5%'}}>
+                    <CardTitle title={this.state.post.title}/>
+                    <h3 style={h3Style}>{this.state.post.authorUsername}</h3>
+                    <h4 style={h4Style}></h4>
                     <Card style={{width:'102%', marginLeft:'-1%'}}>
-                        <img src={"https://www.gapa.de/website/var/tmp/image-thumbnails/0/4284/thumb__gapaWysiwygImageRight/Wandern@2x.jpeg"}
+                        <img src={"https://images.pexels.com/photos/771079/pexels-photo-771079.jpeg?auto=compress&cs=tinysrgb&h=350"}
                              style={{width:'100%', objectFit:'cover', maxHeight:'400px'}}/>
                     </Card>
                     <CardText>
-                        <p> <b>Lorem Ipsum is awesome</b></p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum elementum tortor quis mattis. Fusce ac mi aliquam, volutpat nulla id,
-                            pellentesque lacus. Quisque vel dolor ipsum. Donec non ante ex. Nulla interdum quis nisl mattis molestie. Cras vestibulum nisi a nisl
-                            vulputate maximus. Proin eleifend tempor est eget cursus. Aliquam erat volutpat. Ut mollis, orci sit amet auctor iaculis, nunc lectus
-                            euismod tortor, id fringilla leo velit eu neque. Aliquam libero eros, semper ut justo eu, egestas porttitor arcu. Nullam est libero,
-                            ullamcorper et condimentum a, egestas qui:ws mi. Proin iaculis vel mauris quis iaculis. Vivamus ac aliquam nibh. Nullam quis est tellus.
-                        </p>
+                        <p> <b>{this.state.post.content.split(".").slice(0,2).join(".")}. </b></p>
+                        <p>{this.state.post.content.split(".").slice(2).join(".")}. </p>
                         <FacebookShareButton style={shareStyle} url={url}>
-                            <FacebookIcon
-                                size={32} round />
+                            <FacebookIcon size={32} round />
                         </FacebookShareButton>
                         <TwitterShareButton style={shareStyle} url={url}>
-                            <TwitterIcon
-                                size={32} round />
+                            <TwitterIcon size={32} round />
                         </TwitterShareButton>
                         <LinkedinShareButton style={shareStyle} url={url}>
-                            <LinkedinIcon
-                                size={32} round />
+                            <LinkedinIcon size={32} round />
                         </LinkedinShareButton>
                         <GooglePlusShareButton style={shareStyle} url={url}>
-                            <GooglePlusIcon
-                                size={32} round />
+                            <GooglePlusIcon size={32} round />
                         </GooglePlusShareButton>
 
                     </CardText>
